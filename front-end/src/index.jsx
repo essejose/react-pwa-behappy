abca3720c188ec4fa7c26d23773c76ad71cb82d2
essejose <ze_pk2@hotmail.com>
2018-08-28 11:22:10 -0300

    criando componente header

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,11 +1,12 @@
 import	React from 'react'
 import	ReactDOM from 'react-dom'
+import 	App from './components/App'
 import	'./img/favicon.ico';
 import	'./css/index.css';
 import	'./css/pure-min.css';
 
 
 ReactDOM.render(
-			<h1>Bem-vindo	ao	React!</h1>,
-			document.querySelector("#main")
+	<App/>,
+	document.querySelector("#main")
 )
